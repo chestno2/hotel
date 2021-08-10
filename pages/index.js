494ebc7612427b2722b6f48c400b2a7cad67da10
chestno.02 <atuly152001@gmail.com>
2021-08-11 01:18:08 +0530

    Banner made

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Head from 'next/head'
+import Banner from '../Components/Banner'
 import Header from '../Components/Header'
 
-export default function Home() {
+export default function Home({exploreData}) {
   return (
     <div className="">
       <Head>
@@ -12,7 +13,31 @@ export default function Home() {
       {/* Header */}
        <Header/>
             {/* Banner */}
-      
+      <Banner/>
+
+      <main className=" max-w-7xl mx-auto px-8 sm:px-16 " >
+        <section className="pt-6 " >
+          <h2 className=" text-4xl font-semibold pb-5" >Explore Nearby</h2>
+       
+       {/* Pull Data from an api */}
+           {exploreData.map(item=>(
+             <h1>{item.location}</h1>
+           ))}
+        </section>
+      </main>
     </div>
   )
+}
+export async function getStaticProps()
+{
+   const exploreData = await fetch("https://links.papareact.com/pyp").
+   then((res)=>res.json()
+   
+   );
+   return {
+     props:{
+       exploreData
+     }
+   }
+
 }
